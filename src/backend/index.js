6a85e7e3f03c13b2c988d804b0afe1343893f6ac
Exaedro <owomolo123@gmail.com>
2025-06-14 20:17:03 -0300

    index.js: cookie-parser añadido

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors'
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
@@ -8,7 +9,11 @@ app.set('port', process.env.API_PORT || 3000);
 
 // Middlewares
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
+app.use(cookieParser());
 
 // Importar rutas
 import userRouter from './routes/user.routes.js';
